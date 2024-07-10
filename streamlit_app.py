db91ca0f5365b841350b39030e553c5698a2f63b
Tom Cruse <31087589+firefirefire1@users.noreply.github.com>
2024-07-10 16:27:58 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ with col1:
         "Choose a model:",
         options=list(models.keys()),
         format_func=lambda x: models[x]["name"],
-        index=4  # Default to mixtral
+        index=2  # Default to mixtral
     )
 
 # Detect model change and clear chat history if model has changed
